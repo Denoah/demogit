f0640504ed87800406ae15488d9b0059757d2101
Alexandr Ivanov <ivanov.a@i-teco.ru>
2021-12-09 00:11:22 +0300

    badcommit

--- src/main/java/com/example/demogit/DemogitApplication.java
+++ src/main/java/com/example/demogit/DemogitApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemogitApplication {
 
     public static void main(String[] args) {
+        System.out.println(1);
         SpringApplication.run(DemogitApplication.class, args);
     }
 
