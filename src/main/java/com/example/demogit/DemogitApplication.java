05f80b7956424f3d97887c3d703d46f38e0049f9
Alexandr Ivanov <ivanov.a@i-teco.ru>
2021-12-09 00:17:51 +0300

    bad commimt2

--- src/main/java/com/example/demogit/DemogitApplication.java
+++ src/main/java/com/example/demogit/DemogitApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemogitApplication {
 
     public static void main(String[] args) {
-        System.out.println(1);
+        System.out.println(12);
         SpringApplication.run(DemogitApplication.class, args);
     }
 
